4698c735e73baa01cfa1ec1e3cc201c2fd986def
ItsMaipianX <122278896+ItsMaipianX@users.noreply.github.com>
2025-06-06 14:38:20 +0800

    Update Loader.php

--- src/Zinkil/AntiWin10/Loader.php
+++ src/Zinkil/AntiWin10/Loader.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Zinkil\AntiWin10;
 
@@ -13,15 +13,15 @@ use pocketmine\event\Listener;
 
 class Loader extends PluginBase implements Listener{
 
-    public function onEnable(){
+    public function onEnable() : void{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
-    public function DataPacketReceive(DataPacketReceiveEvent $event){
+    public function DataPacketReceive(DataPacketReceiveEvent $event) : void{
         $player = $event->getPlayer();
         $packet = $event->getPacket();
-        if ($packet instanceof LoginPacket){
-            if ($packet->clientData["DeviceOS"] == 7){ //7 is for Windows10 OS
+        if($packet instanceof LoginPacket){
+            if($packet->clientData["DeviceOS"] === 7){ //7 is for Windows10 OS
                 $player->kick("§l§eYou are playing on Windows 10\n§r§cYou can't join this server with Win10", false);
             }
         }
